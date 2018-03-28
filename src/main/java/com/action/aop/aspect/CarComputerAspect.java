58b314b02a342e2406f3ee66f1be373ccbe55a45
Bogdan Marchis <bogdan.marchis@cleverhouse-engineering.com>
2018-03-28 16:49:20 +0300

    Performance test using AOP

--- src/main/java/com/action/aop/aspect/CarComputerAspect.java
+++ src/main/java/com/action/aop/aspect/CarComputerAspect.java
@@ -1,7 +1,12 @@
 package com.action.aop.aspect;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -28,5 +33,19 @@ public class CarComputerAspect {
 	@After("annotationExampleAOPUsageInterfacePointCutDefinition() && atExecution()")
 	public void logCarStopAdvice(JoinPoint joinPoint){
 		System.out.println("AOP AD: Stopping the engine of the car....");
+	}
+	
+	@Around("annotationExampleAOPUsageInterfacePointCutDefinition() && atExecution()")
+	public void calculateTimePerformance(ProceedingJoinPoint joinPoint){
+		LocalDateTime ldt = LocalDateTime.now();
+		System.out.println("Started the performance test for method:"+joinPoint.toLongString()+" at time:"+ldt.toString());
+		try {
+			joinPoint.proceed();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		LocalDateTime ldt2 = LocalDateTime.now();
+		System.out.println("Stopped the performance test for method:"+joinPoint.toLongString()+" at time:"+ldt2.toString());
+		System.out.println("Time elapsed:"+ChronoUnit.MILLIS.between(ldt,ldt2)+ " ms");
 	}
 }
